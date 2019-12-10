cfa25ebdb85801f95a47af420e385c023cc01d3d
Stella <feelis1119@naver.com>
2019-12-09 23:46:05 -0500

    fixing

--- app.js
+++ app.js
@@ -23,19 +23,16 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+app.use((req, res, next) => {
+  var err = new Error('Not Found!');
+  err.status = 404;
+  err.customMessage = "Something terrible happened, You page has gone missing!"
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  next(err);
+})
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.use((err, req, res, next) => {
+  res.render('error', {data: err, layout: 'errorPage'})
+})
 
 module.exports = app;
